1b1a741b7563ed5aa914e8bcaa9a86f2c7e57ef9
whyssem <155022002+whyssem@users.noreply.github.com>
2024-12-24 19:17:21 +0100

    Create exo2.py

---
+++ exo2.py
@@ -0,0 +1,9 @@
+degre = int(input("entrez le degree d'aujourd'hui : "))
+if degre <= 0 :
+    print("il va faire tres froid aujourd'hui !")
+
+elif degre > 0 and degre <= 10 :
+    print("il va faire froid aujourd'hui! ")
+
+elif degre <= 20 and degre > 10 :
+    print ("il va faire frais aujourd'hui! ")
